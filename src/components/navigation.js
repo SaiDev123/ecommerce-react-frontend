3380eed9c53e2feeff05825d492b46872351e760
sai <sai@digital-lync.com>
2021-03-10 10:57:10 +0530

    added register

---
+++ src/components/navigation.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {NavLink,Route} from 'react-router-dom';
+import Register from '../containers/register';
+
+class Navigation extends React.Component{
+    render()
+{
+    return(
+            <React.Fragment>
+            <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+    <ul className="navbar-nav">
+    <li className="nav-item">
+     <NavLink className="nav-link" to="/">Home</NavLink>
+    </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/login">Login</NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink className="nav-link" to="/register">Register</NavLink>
+      </li>
+      <li className="nav-item">
+      <NavLink className="nav-link" to="/careers">Careers</NavLink>
+    </li>
+    <li className="nav-item">
+    <form className="form-inline">
+        <input className="form-control mr-sm-2" type="text" placeholder="Search"/>
+        <button className="btn btn-success" type="submit">Search</button>
+      </form>
+    </li>
+    </ul>
+  </nav>
+
+     <Route path="/register" component={Register}></Route>
+      
+  </React.Fragment>
+    )
+}
+}
+export default Navigation;
